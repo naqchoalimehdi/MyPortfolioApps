b22bdcfb3c779f2c273040abd37605fbd7295ed1
Naqcho Ali Mehdi <88571499+naqchoalimehdi@users.noreply.github.com>
2025-03-28 23:55:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from langchain_groq import ChatGroq
+from pydantic import BaseModel
+from langchain.prompts import PromptTemplate
+import os
 
 app = FastAPI()
+
+llm = ChatGroq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+    max_retries=2,
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+class ChatRequest(BaseModel):
+    user_input1: str
+
+@app.post("/chatbot")
+async def chat(request: ChatRequest):
+    try:
+        prompt = "you are a comedian and you tell jokes about {user_input}."
+        prompt_template = PromptTemplate.from_template(prompt)
+        formatted_prompt = prompt_template.invoke({"user_input": request.user_input1}) #Corrected line
+        response = llm.invoke(formatted_prompt)
+        return {"response": response.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
